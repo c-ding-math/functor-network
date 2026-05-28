a8f50deaaa4457b625afcf652700ca9da28d7773
Ding, C. <c_ding@foxmail.com>
2026-05-28 09:55:11 +0800

    csl indent

--- src/Handler/Entries.hs
+++ src/Handler/Entries.hs
@@ -74,19 +74,19 @@ getEntriesR = do
 entryListWidget :: Text ->  [Entity Entry] -> Widget
 entryListWidget style entryList = do
     listData <- handlerToWidget $ do
-        (authors, categoryLists)<-runDB $ do
+        {-(authors, categoryLists)<-runDB $ do
             authors <- mapM (get404 . entryUserId . entityVal) entryList
             categoryLists <- forM entryList $ \entry -> do
                 let entryId = entityKey entry
                 entryCategoryIds<- map (entryTreeParent . entityVal) <$> selectList [EntryTreeNode ==. entryId] []
                 selectList [EntryId <-. entryCategoryIds] [Desc EntryInserted]
-            return (authors, categoryLists)
-        
-        return $ zip3 entryList authors categoryLists
+            return (authors, categoryLists)-}
+        authors <- runDB $ mapM (get404 . entryUserId . entityVal) entryList
+        return $ zip entryList authors
 
     [whamlet|
 <ul .list-group .entry-list .#{style}>
-    $forall (Entity entryId entry, author, entryCategoryEntities)<- listData
+    $forall (Entity entryId entry, author)<- listData
         <li .list-group-item .entry-item :entryStatus entry == Draft:.draft>
             <div .entry-meta>
                 <ul.list-inline>
@@ -109,21 +109,6 @@ entryListWidget style entryList = do
                                     <li>
                                         <a .text-muted onclick="return false;" href=#>
                                             updated #{utcToDateTime (entryUpdated entry)}
-                    $if not (null entryCategoryEntities)
-                        <li .in>
-                            <span title="category"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder" viewBox="0 0 16 16"><path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139q.323-.119.684-.12h5.396z"/></svg>
-                            <span .dropdown>
-                                <a .text-muted.dropdown-toggle data-toggle=dropdown>
-                                    #{show $ length entryCategoryEntities} #
-                                    $if length entryCategoryEntities <= 1
-                                        category 
-                                    $else
-                                        categories 
-                                    <span .caret>
-                                <ul .dropdown-menu>
-                                    $forall (Entity categoryId category)<-entryCategoryEntities
-                                        <li>
-                                            <a href=@{CategoriesR (entryUserId category)}#{"#entry-" <> toPathPiece categoryId}>#{preEscapedToMarkup $ entryTitleHtml category}
 
             <a.stretched-link href=@{UserEntryR (entryUserId entry) entryId}>
                 <h3 .entry-title>#{preEscapedToMarkup(scaleHeader 3 (entryTitleHtml entry))}
